c486c252f1593abfe395b651b0d17ce31469faec
joshua-stevens <joshan9na@gmail.com>
2020-02-19 11:31:41 -0500

    Create Boss.cs

---
+++ Boss.cs
@@ -0,0 +1,45 @@
+﻿using System;
+
+public class Boss
+{
+    public string name;
+    public int health;
+    public int attack;
+    
+
+	public Boss()
+	{
+        this.name = "Big Boss";
+        this.health = 80;
+        this.attack = 10;
+        
+	}
+    public Boss(string name, int health, int attack)
+    {
+        this.name = name;
+        this.health = health;
+        this.attack = attack;
+        
+    }
+    public override string ToString()
+    {
+        string s1 = "";
+        s1 += "Name: " + this.name
+            + " Attack: " + this.attack
+            + " Health: " + this.health;
+           
+        return s1;
+    }
+    public void attackHero(Hero h1)
+    {
+        h1.health -= this.attack;
+        
+    }
+    public void attacked(Hero h1)
+    {
+        this.health -= h1.attack;
+        
+    }
+    
+
+}
